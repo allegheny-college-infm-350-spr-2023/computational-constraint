d5999b1145bfbdd817f5e290f14d7169a7a0729d
dluman <dluman@allegheny.edu>
2023-01-22 22:08:15 +0000

    Adding changes to main to finish GPT-3 process

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ OPEN_AI_ORG = CONFIG["ORG"] or os.environ["OPEN_AI_ORG"]
 openai.api_key = OPEN_AI_KEY
 openai.organization = OPEN_AI_ORG
 
-def load_files(filename: str = "") -> str:
+def load_file(filename: str = "") -> str:
     with open(filename, "r") as fh:
         return fh.read()
 
@@ -25,17 +25,20 @@ def main():
     source = load_file("data/source.txt")
     prompt = load_file("data/prompt.txt")
 
-    # Calculate size of spent tokens
-    used_tokens = len(source) - len(prompt)
+    # Create constraint
+    constraint = f"{prompt}:\n\n{source}"
 
     response = openai.Completion.create(
         model = "text-davinci-003",
         prompt = constraint,
-        temperature = 0.1,
-        max_tokens = (4097 - used_tokens),
+        temperature = 0.9,
+        max_tokens = (4097 - len(constraint)),
         frequency_penalty = 2.0,
         presence_penalty = -2.0
     )
+
+    for choice in response["choices"]:
+        print(choice["text"].strip())
 
 if __name__ == "__main__":
     main()
